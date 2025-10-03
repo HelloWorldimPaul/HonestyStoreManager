8f8eb698b3eef371a82f5098e6335c1736979ce5
HelloPaul <Paulpedrano@gmail.com>
2025-10-03 19:22:23 +0800

    Add initial User, Product, and Transaction model classes

--- src/main/java/honestystore/models/Transaction.java
+++ src/main/java/honestystore/models/Transaction.java
@@ -1,4 +1,23 @@
 package honestystore.models;
 
 public class Transaction {
+
+    private int id;
+    private String userName;
+    private String productName;
+    private int quantity;
+    private double amount;
+    private String type;
+    private String date;
+
+    public Transaction(int id, String userName, String productName, int quantity, double amount, String type, String date) {
+        this.id = id;
+        this.userName = userName;
+        this.productName = productName;
+        this.quantity = quantity;
+        this.amount = amount;
+        this.type = type;
+        this.date = date;
+    }
+
 }
--- src/main/java/honestystore/models/User.java
+++ src/main/java/honestystore/models/User.java
@@ -1,4 +1,24 @@
 package honestystore.models;
 
 public class User {
+
+    private String name;
+    private int totalDebt;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTotalDebt() {
+        return this.totalDebt;
+    }
+
+    public void setTotalDebt(int totalDebt) {
+        this.totalDebt = totalDebt;
+    }
+
 }
